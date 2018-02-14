d7a044b12444308a90f9a2412db413163be5400f
ORPHANED <cran@R-project.org>
2018-02-14 10:53:11 +0000

    version 0.10-1.1

---
+++ src/init.c
@@ -0,0 +1,40 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .C calls */
+extern void TikZ_Annotate(void *, void *, void *);
+
+/* .Call calls */
+extern SEXP TikZ_DeviceInfo(SEXP);
+extern SEXP TikZ_EvalWithoutInterrupts(SEXP, SEXP);
+
+/* .External calls */
+extern SEXP TikZ_StartDevice(SEXP);
+
+static const R_CMethodDef CEntries[] = {
+    {"TikZ_Annotate", (DL_FUNC) &TikZ_Annotate, 3},
+    {NULL, NULL, 0}
+};
+
+static const R_CallMethodDef CallEntries[] = {
+    {"TikZ_DeviceInfo",            (DL_FUNC) &TikZ_DeviceInfo,            1},
+    {"TikZ_EvalWithoutInterrupts", (DL_FUNC) &TikZ_EvalWithoutInterrupts, 2},
+    {NULL, NULL, 0}
+};
+
+static const R_ExternalMethodDef ExternalEntries[] = {
+    {"TikZ_StartDevice", (DL_FUNC) &TikZ_StartDevice, 21},
+    {NULL, NULL, 0}
+};
+
+void R_init_tikzDevice(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, CallEntries, NULL, ExternalEntries);
+    R_useDynamicSymbols(dll, FALSE);
+}
